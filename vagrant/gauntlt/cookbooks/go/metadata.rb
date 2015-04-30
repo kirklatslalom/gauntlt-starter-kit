e7d13bb55d12b06e7a110b6406d86430d3821aa5
kirkl <kirkl@slalom.com>
2015-04-30 16:26:28 -0700

    Need go too

---
+++ vagrant/gauntlt/cookbooks/go/metadata.rb
@@ -0,0 +1,7 @@
+name             'go'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures go'
+long_description 'Installs/Configures go'
+version          '0.1.0'
